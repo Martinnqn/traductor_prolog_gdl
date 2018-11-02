3c7f76c1569b94159e7971ed1d94fcbfd6dfa411
Martinnqn <bermudez_martin.nqn@hotmail.com>
2018-11-02 11:51:20 -0300

    eliminado archivos compilacion

--- trad_gdl.pl
+++ trad_gdl.pl
@@ -5,108 +5,120 @@
 %package para traducir el programa.
 :- use_package(.(meta_trans)).
 
-%en este archivo tiene que ir el codigo prolog para traducir a gdl
+%para ejecutar ?-inicio,juego. (Run!)
 
-gg:-A is 4+2, gg1,gg2, cell_size(A,B).
+%roles o y x
+role(o).
+role(x).
 
+%estado inicial
+init(cell(1,1,b)).
+init(cell(1,2,b)).
+init(cell(1,3,b)).
+init(cell(2,1,b)).
+init(cell(2,2,b)).
+init(cell(2,3,b)).
+init(cell(3,1,b)).
+init(cell(3,2,b)).
+init(cell(3,3,b)).
+init(control(o)).
 
+%posibles valores que pueden tener las relaciones
+base(control(X)):- role(X).
+base(cell(X,Y,b)):- index(X),index(Y).
+base(cell(X,Y,R)):- role(R),index(X),index(Y).
+
+index(1).
+index(2).
+index(3).
+
+%posibles valores que pueden tener las entradas
+input(R,mark(X,Y)):- role(R),index(X),index(Y).
+input(R,noop):-role(R).
+
+%movimientos legales
 legal(W,mark(X,Y)) :-
 	t(cell(X,Y,b)),t(control(W)).
+legal(o,noop) :-
+	t(control(x)).
+legal(x,noop) :-
+      t(control(o)).
 
-%%%%codigo de ejemplo%%%%%
-% init(control(x)).
+%próximo estado
+next(cell(M,N,x)) :-
+      does(x,mark(M,N)),
+      t(cell(M,N,b)).
 
-% %posibles valores que pueden tener las relaciones
-% base(cell(X,Y,b)) :- 
-%   index(X),
-%   index(Y).
+next(cell(M,N,o)) :-
+      does(o,mark(M,N)),
+      t(cell(M,N,b)).
 
-% base(cell(X,Y,p)) :- 
-%   index(X),
-%   index(Y).
+next(cell(M,N,W)) :-
+      t(cell(M,N,W)),
+      distinct(W,b).
 
-% base(cell(X,Y,rol)) :- 
-%   role(R),
-%   index(X),
-%   index(Y).
+next(cell(M,N,b)) :-
+      does(_W,mark(J,_K)),
+      t(cell(M,N,b)),
+      distinct(M,J).
 
-% base(cell(X,Y,pelota(R,P,XV,YV))) :- 
-%   role(R),
-%   pelota(P),
-%   index(X),
-%   index(Y),
-%   velocidad(XV),
-%   velocidad(YV).
+next(cell(M,N,b)) :-
+      does(_W,mark(_J,K)),
+      t(cell(M,N,b)),
+      distinct(N,K).
 
-% base(cell(X,Y,golpeado(R))) :- 
-%   role(R),
-%   index(X),
-%   index(Y).
+next(control(o)) :-
+      t(control(x)).
 
-% base(pelotas(R,C)) :-
-%   role(R),
-%   count_pelota(C).
+next(control(x)) :-
+      t(control(o)).
 
-% base(control(R)) :- 
-%   role(R).
+%Cambiamos las reglas pierde el que hace TATETI
+%goal(x,100) :- line(x),\+line(o).
+%goal(x,50) :-  \+line(x), \+line(o).
+%goal(x,0) :- \+line(x), line(o).
 
-% velocidad(0).
-% velocidad(1).
-% velocidad(-1).
+%goal(o,100) :- \+line(x), line(o).
+%goal(o,50) :- \+line(x), \+line(o).
+%goal(o,0) :- line(x), \+line(o).
 
-% %posibles valores que pueden tener las entradas
-% input(R,lanzar(X,Y)) :- 
-%   role(R),
-%   index(X),
-%   index(Y).
+goal(x,0) :- line(x),\+line(o).
+goal(x,50) :-  \+line(x), \+line(o).
+goal(x,100) :- \+line(x), line(o).
 
-% input(R,mover(X,Y)) :- 
-%   role(R),
-%   index(X),
-%   index(Y).
+goal(o,0) :- \+line(x), line(o).
+goal(o,50) :- \+line(x), \+line(o).
+goal(o,100) :- line(x), \+line(o).
 
-% input(R,nada) :- 
-%   role(R).
 
-% %movimientos legales
-% legal(R,lanzar(X,Y)) :- 
-%   t(control(R)),
-%   (t(cell(X,Y,b));t(cell(X,Y,rol(_R1)))),
-%   \+(posicion_futura(_R,_P,X,Y)),
-%   t(cell(X1,Y1,rol(R))),
-%   adyacente(X,Y,X1,Y1),
-%   t(pelotas(R,C)),
-%   C > 0.
+line(Z) :- row(_M,Z).
+    line(Z) :- column(_M,Z).
+    line(Z) :- diagonal(Z).
 
-% legal(R,mover(X,Y)) :- 
-%   t(control(R)),
-%   t(cell(X,Y,pelota(_R1,_P,_XV,_YV))),
-%   t(cell(X1,Y1,rol(R))),
-%   adyacente(X,Y,X1,Y1).
+    row(M,Z) :-
+      t(cell(M,1,Z)) ,
+      t(cell(M,2,Z)) ,
+      t(cell(M,3,Z)).
 
-% legal(R,mover(X,Y)) :- 
-%   t(control(R)),
-%   t(cell(X,Y,b)),
-%   t(cell(X1,Y1,rol(R))),
-%   adyacente(X,Y,X1,Y1).
+    column(_M,Z) :-
+      t(cell(1,N,Z)) ,
+      t(cell(2,N,Z)) ,
+      t(cell(3,N,Z)).
 
-% legal(_R,nada).
+    diagonal(Z) :-
+      t(cell(1,1,Z)) ,
+      t(cell(2,2,Z)) ,
+      t(cell(3,3,Z)).
 
-% adyacente(X1,Y1,X2,Y2) :-
-%   ((X1 is X2+1; X1 is X2-1),(Y1 is Y2+1; Y1 is Y2-1));
-%   ((X1 is X2),(Y1 is Y2+1; Y1 is Y2-1));
-%   ((X1 is X2+1; X1 is X2-1),(Y1 is Y2)).
+    diagonal(Z) :-
+      t(cell(1,3,Z)) ,
+      t(cell(2,2,Z)) ,
+      t(cell(3,1,Z)).
 
-% %prÃ³ximo estado
-% %celdas con paredes
-% next(cell(X,Y,p)) :- 
-%   t(cell(X,Y,p)).
+ terminal :- line(x).
+ terminal :- line(o).
+ terminal :- \+open.
 
-% %celdas con jugador golpeado
-% %si se mueve hacia una pelota enemiga
-% next(cell(X,Y,golpeado(R))) :-
-%   role(R),
-%   does(R,mover(X,Y)),
-%   posicion_futura(E,_P,X,Y),
-%   E \== R,
-%   \+colision(X,Y).
+ open :- t(cell(X,Y,b)),
+ index(X),
+ index(Y).
